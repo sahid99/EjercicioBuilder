1ae5083485cf4312f9abc74eeb6edffc54cab879
Sahid Enrico De la Rosa Cruz <sahid.enrico@gmail.com>
2020-02-20 18:15:16 -0600

    Agregar archivos de proyecto.

---
+++ EjercicioBuilder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EjercicioBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
